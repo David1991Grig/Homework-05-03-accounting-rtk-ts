035612ada4b59150aa8478371f4b98649248b278
David1991Grig <davoaleks1991@gmail.com>
2025-03-08 12:20:25 +0200

    Homework

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,4 @@
 export const base_url = 'https://webaccounting.herokuapp.com/account';
 export const createToken = (login:string, password:string) => `Basic ${btoa(login+':'+password)}`;
-export const createTokenPassword = (password:string) => `Basic ${btoa(password)}`;
-export const decodeTokenPassword = (token: string) => {
-    const base64String = token.replace('Basic ', '');
-    return atob(base64String);
-};
+export const createTokenPassword = (password:string) => `${btoa(password)}`;
+export const decodeTokenPassword = (token: string) =>  atob(token);
